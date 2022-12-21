92657acaea5833e296a747f5f49aa8269a5a6056
t3j3ndrA <tejendradhanani@gmail.com>
2022-12-21 21:01:44 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { useCookies } from "react-cookie";
+import axios from "axios";
+axios.defaults.withCredentials = true;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [cookies, setCookie, removeCookie] = useCookies();
+	const serverUri = process.env.server_base_uri;
+	console.log("serveruri = ", serverUri);
+	const logIn = () => {
+		axios
+			.post("http://localhost:5000/")
+			.then((response) => response.data)
+			.then((data) => {
+				console.log(data);
+				setCookie("logged-in-user", data?.uid);
+			})
+			.catch((errr) => console.log(errr));
+	};
+
+	const handleLogOut = () => {
+		removeCookie("logged-in-user");
+	};
+
+	console.log("cookie : ", cookies);
+	useEffect(() => {
+		setCookie("initial", "initial-cookie");
+	}, []);
+
+	const handleGet = () => {
+		axios
+			.get("http://localhost:5000/")
+			.then((resp) => resp.data)
+			.then((data) => console.log(data));
+	};
+	return (
+		<div>
+			<p>App</p>
+			<button
+				onClick={() => {
+					logIn();
+				}}
+			>
+				Log in
+			</button>
+			<button
+				onClick={() => {
+					handleLogOut();
+				}}
+			>
+				Log out
+			</button>
+			<button
+				onClick={() => {
+					handleGet();
+				}}
+			>
+				Set get cookie
+			</button>
+		</div>
+	);
 }
 
 export default App;
