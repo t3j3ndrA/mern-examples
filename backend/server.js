48a2c71c99392d742e860fcf93d92378f9f96e2d
t3j3ndrA <tejendradhanani@gmail.com>
2022-12-22 22:09:17 +0530

    deployment fix-2

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,9 @@
 const express = require("express");
+const app = express();
 const env = require("dotenv");
-const path = require("path");
 
 env.config();
-
-const app = express();
+const path = require("path");
 const port = process.env.PORT || 5000;
 
 app.listen(port, () =>
@@ -21,9 +20,7 @@ app.get("/api", (req, res) => {
 	res.json({ msg: "server is up and running" });
 });
 
-// for the deployment
-console.log("directory name : " + __dirname);
 app.use(express.static(path.join(__dirname, "/frontend/build")));
 app.get("*", (req, res) =>
-	res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+	res.sendFile(path.join(__dirname, "..", "/frontend/build/index.html"))
 );
